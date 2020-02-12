65b224684e651cfbb8676d173c5f700efecf7f3b
surajnetsol <58321490+surajnetsol@users.noreply.github.com>
2020-02-12 18:22:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+function formatName(user) {
+  console.log(user);
+  return user.firstname + " " + user.lastname;
+}
+const user = {
+  firstname: "Suraj",
+  lastname: "Singh"
+};
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const element = <h1>Hello, {formatName(user)}</h1>;
+  return element;
 }
 
 export default App;
